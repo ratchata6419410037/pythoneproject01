196500cec842a9d8c9f76002ec3c5be349acbc3e
ratchata6419410037 <s6419410037@sau.ac.th>
2023-08-18 11:16:11 +0700

    code v.1 : creat 3 file

---
+++ test02.py
@@ -0,0 +1,7 @@
+print("Hi....", end='')
+print('Hey...')
+print(447655)
+print(True)
+print('False')
+print("7908543")
+print(10+10+20)
---
+++ test03.py
@@ -0,0 +1,18 @@
+print('AAA'); print("BBB")
+print("CCC")
+print("DDD\nEEE\nFFF")
+print("DDD\tEEE\tFFF")
+print('Welcome "SAU"')
+print("Welcome 'SAU'")
+print("Welcome \"SAU\"")
+print('Welcome \'SAU\'')
+print('ABC\\DEF')
+print('''
+      SAU...
+      HI...
+      WOW...
+      IOT...
+      ''')
+    
+     
+
